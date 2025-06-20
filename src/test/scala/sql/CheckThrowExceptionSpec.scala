9dcf7a1b953571c645db0e112b7a19210b2586f2
zhaihao <zhaihao@ooon.me>
2025-06-20 15:25:23 +0800

    SQL 抛异常 测试pass

--- src/test/scala/sql/CheckThrowExceptionSpec.scala
+++ src/test/scala/sql/CheckThrowExceptionSpec.scala
@@ -9,36 +9,45 @@ package me.ooon.sparkia
 package sql
 
 import com.typesafe.scalalogging.StrictLogging
+import org.apache.spark.SparkException
 //noinspection SqlNoDataSourceInspection
 //language=SQL
 class CheckThrowExceptionSpec extends SparkSpec with StrictLogging {
   import spark.implicits._
-  "test" in {
-    val df = Seq(
-      (1, 1),
-      (2, 2)
-    ).toDF("c1", "c2")
-    df.show()
-    df.createOrReplaceTempView("test")
 
-    // 必须 show 才会报错
+  val df = Seq(
+    (1, 1),
+    (2, 2)
+  ).toDF("c1", "c2")
+  df.show()
+  df.createOrReplaceTempView("test")
 
-    // 高版本, 有内置函数
-    spark.sql("select RAISE_ERROR('error!')").show()
+  // 必须 show 才会报错
 
-    //  低版本
-    spark.sql(
-      """
-        |
-        |select assert_true((select count(1) as cnt from test) > 1) as check;
-        |
-        |""".stripMargin).show()
+  "高版本内置函数" in {
+    assertThrows[SparkException] {
+      spark.sql("select RAISE_ERROR('error!')").show()
+    }
 
-    spark.sql(
-      """
-        |
-        |select assert_true((select count(1) as cnt from test) > 10) as check;
-        |
-        |""".stripMargin).show()
+  }
+
+  // 可以自定义一个 udf RAISE_ERROR，或者使用 assert_true
+  "低版本" in {
+    spark
+      .sql("""
+             |select assert_true((select count(1) as cnt from test) > 1) as check;
+             |
+             |""".stripMargin)
+      .show()
+
+    assertThrows[SparkException] {
+      spark
+        .sql("""
+               |
+               |select assert_true((select count(1) as cnt from test) > 10) as check;
+               |
+               |""".stripMargin)
+        .show()
+    }
   }
 }
