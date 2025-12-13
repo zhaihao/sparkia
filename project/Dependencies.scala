acb1ce6899373ee20c864c38cc8dffb85a0908c6
zhaihao <zhaihao@ooon.me>
2025-12-13 01:50:12 +0000

    update: logback-classic 1.5.21 -> 1.5.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies extends AutoPlugin {
     lazy val LOG = Seq(
       "org.slf4j"                   % "log4j-over-slf4j" % "2.0.17",
       "com.typesafe.scala-logging" %% "scala-logging"    % "3.9.6",
-      "ch.qos.logback"              % "logback-classic"  % "1.5.21"
+      "ch.qos.logback"              % "logback-classic"  % "1.5.22"
     )
 
     lazy val SCALA_TEST = Seq(
