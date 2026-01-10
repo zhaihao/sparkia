25071d923a8184b944ee9f34c382a8820e3c3b8f
zhaihao <zhaihao@ooon.me>
2026-01-10 01:57:23 +0000

    update: spark-core, spark-sql 4.1.0 -> 4.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,8 +43,8 @@ object Dependencies extends AutoPlugin {
     )
 
     lazy val SPARK = Seq(
-      "org.apache.spark" %% "spark-core" % "4.1.0",
-      "org.apache.spark" %% "spark-sql"  % "4.1.0"
+      "org.apache.spark" %% "spark-core" % "4.1.1",
+      "org.apache.spark" %% "spark-sql"  % "4.1.1"
     )
 
     lazy val overrides = Seq(
